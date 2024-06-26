5d61a5edb1d8a2444731ed9bcd105c4772bc3cce
Juan Vidal <juanmvg2003@gmail.com>
2024-06-26 13:13:11 -0500

    cambios en el cors para acceder desde flutter

--- src/index.js
+++ src/index.js
@@ -24,7 +24,7 @@ const port = process.env.PORT || 8000;
 
 app.use(express.json());
 app.use(cors({
-    origin: process.env.FRONTEND_URL,
+    origin: [process.env.FRONTEND_URL, process.env.MOVIL_URL],
     credentials: true
 }));
 app.use(cookieParser());
