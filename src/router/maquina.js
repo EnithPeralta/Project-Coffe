95f32575e68c69bbe2e71f1239933d86ba39de45
LauraB1903 <laurabastidas0319@gmail.com>
2024-07-17 12:49:54 -0500

    cambiar sintaxis en el controlador maquina

--- src/router/maquina.js
+++ src/router/maquina.js
@@ -3,48 +3,109 @@ const maquinaSchema = require("../models/maquina");
 const routes = express.Router();
 
 // Create máquina
-routes.post('/maquinas', (req, res) => {
-    const maquina = maquinaSchema(req.body);
-    maquina.save()
-        .then((data) => res.json(data))
-        .catch((error) => res.status(400).json({ message: error.message }));
+
+routes.post('/maquinas', async (req, res) => {
+    try {
+    const maquina = new maquinaSchema(req.body); 
+    const savedMaquina = await maquina.save();
+    res.json(savedMaquina);
+    } catch (error) {
+    res.status(400).json({ message: error.message });
+    }
 });
+
+
+// routes.post('/maquinas', (req, res) => {
+//     const maquina = maquinaSchema(req.body);
+//     maquina.save()
+//         .then((data) => res.json(data))
+//         .catch((error) => res.status(400).json({ message: error.message }));
+// });
 
 // Get all máquinas
-routes.get('/maquinas', (req, res) => {
-    maquinaSchema
-    .find()
-    .then((data) => res.json(data))
-    .catch((error) => res.status(400).json({ message: error.message }));
-});
+
+routes.get('/maquinas', async (req, res) => {
+    try {
+      const maquinas = await maquinaSchema.find();
+      res.json(maquinas);
+    } catch (error) {
+      res.status(400).json({ message: error.message });
+    }
+  }); 
+
+
+// routes.get('/maquinas', (req, res) => {
+//     maquinaSchema
+//     .find()
+//     .then((data) => res.json(data))
+//     .catch((error) => res.status(400).json({ message: error.message }));
+// });
 
 // Get a máquina by ID
-routes.get('/maquinas/:id', (req, res) => {
-    const { id } = req.params;
-    maquinaSchema
-    .findById(id)
-    .then((data) => res.json(data))
-    .catch((error) => res.status(400).json({ message: error.message }));
-});
+
+routes.get('/maquinas/:id', async (req, res) => {
+    try {
+      const { id } = req.params;
+      const maquina = await maquinaSchema.findById(id);
+      res.json(maquina);
+    } catch (error) {
+      res.status(400).json({ message: error.message });
+    }
+  });
+// routes.get('/maquinas/:id', (req, res) => {
+//     const { id } = req.params;
+//     maquinaSchema
+//     .findById(id)
+//     .then((data) => res.json(data))
+//     .catch((error) => res.status(400).json({ message: error.message }));
+// });
 
 // Update a máquina by ID
-routes.put('/maquinas/:id', (req, res) => {
+
+routes.put('/maquinas/:id', async (req, res) => {
+  try {
     const { id } = req.params;
     const { nombre } = req.body;
-    maquinaSchema
-    .updateOne({_id:id},{$set:{nombre}})
-    .then((data) => res.json(data))
-    .catch((error) => res.status(400).json({ message: error.message }));
+    const updatedMaquina = await maquinaSchema.updateOne({ _id: id }, { $set: { nombre } });
+    res.json(updatedMaquina);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
 });
 
+
+
+
+// routes.put('/maquinas/:id', (req, res) => {
+//     const { id } = req.params;
+//     const { nombre } = req.body;
+//     maquinaSchema
+//     .updateOne({_id:id},{$set:{nombre}})
+//     .then((data) => res.json(data))
+//     .catch((error) => res.status(400).json({ message: error.message }));
+// });
+
 // Delete a máquina by ID
-routes.delete('/maquinas/:id', (req, res) => {
+routes.delete('/maquinas/:id', async (req, res) => {
+  try {
     const { id } = req.params;
-    const { nombre } = req.body;
-    maquinaSchema
-    .deleteOne({_id:id},{$set:{nombre}})
-    .then(() => res.json({ message: 'Máquina eliminada correctamente.' }))
-    .catch((error) => res.status(400).json({ message: error.message }));
+    // No need for 'nombre' from req.body as it's not used in deleteOne
+    await maquinaSchema.deleteOne({ _id: id });
+    res.json({ message: 'Máquina eliminada correctamente.' });
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
 });
+
+
+
+// routes.delete('/maquinas/:id', (req, res) => {
+//     const { id } = req.params;
+//     const { nombre } = req.body;
+//     maquinaSchema
+//     .deleteOne({_id:id},{$set:{nombre}})
+//     .then(() => res.json({ message: 'Máquina eliminada correctamente.' }))
+//     .catch((error) => res.status(400).json({ message: error.message }));
+// });
 
 module.exports = routes;
